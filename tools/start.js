85fe9dd8d6769c959d65bee2adb8bff64817fdd2
Patrick McVeety-Mill <pmcvtm@hey.com>
2022-07-23 14:29:45 -0500

    add start script for testing slides

---
+++ tools/start.js
@@ -0,0 +1,9 @@
+const execSync = require('child_process').execSync;
+const slideDir = process.argv[2] || 'DID-NOT-SPECIFY';
+
+if(slideDir === 'DID-NOT-SPECIFY'){
+    process.stdout.write("ERROR: You must specify a presentation to launch");
+    process.exit();
+}
+
+execSync('reveal-md slides/' + slideDir + '/slides.md -w', {stdio:[0, 1, 2]});
