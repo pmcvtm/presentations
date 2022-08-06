394ec3f8762a4218316ee1be96ed44947f106a4c
Patrick McVeety-Mill <1993790+pmcvtm@users.noreply.github.com>
2022-08-06 12:17:29 -0500

    Initial Setup: Run & Deploy Scripts (#1)
    * add gitignore
    
    * introduce package.json
    
    * install reveal-md dependency
    
    * add start script for testing slides
    
    * add an example deck
    
    * test and fix for images
    
    * test front-matter
    
    * rename main slide to readme so it shows up in GH
    
    * add sample css override
    
    * add script + gh action for publishing content
    
    * add a container home page for published slides

---
+++ slides/example/readme.md
@@ -0,0 +1,32 @@
+﻿---
+title: Example Test Presentation!
+favicon: example/assets/dolphin_hat.JPG
+theme: solarized
+revealOptions:
+  transition: none
+css:
+- overrides.css
+---
+
+# A Presentation
+
+---
+
+## Section 1
+
+Here's a sample
+
+---
+
+A test, really:
+
+- with points
+- to make
+
+---
+
+Test
+
+![a dolphin with sunglasses on its nose](assets/dolphin_hat.JPG)
+
+---
---
+++ tools/publish.js
@@ -0,0 +1,9 @@
+const execSync = require('child_process').execSync;
+const slideDir = process.argv[2] || 'DID-NOT-SPECIFY';
+
+if(slideDir === 'DID-NOT-SPECIFY'){
+    process.stdout.write("ERROR: You must specify a presentation to publish");
+    process.exit();
+}
+
+execSync('cd slides/' + slideDir + ' && reveal-md readme.md --static ../../publish/' + slideDir, {stdio:[0, 1, 2]});
