0a7e3049a2d261f4a9d2c99a33ecd8a812540d61
Patrick McVeety-Mill <pmcvtm@duck.com>
2023-01-09 22:40:12 -0600

    add aquarium endpoints

--- slides/min-maxing-minimal-apis/sample/FishApi/Program.cs
+++ slides/min-maxing-minimal-apis/sample/FishApi/Program.cs
@@ -8,6 +8,32 @@ builder.Services.AddDbContext<FishContext>((_, opt) =>
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/aquariums", (FishContext db) => db.Aquariums.ToList());
+app.MapGet("/aquariums/{id:int}", async (int id, FishContext db) => await db.Aquariums.FindAsync(id));
+app.MapPost("/aquariums", async (Aquarium aquarium, FishContext db) =>
+{
+    await db.AddAsync(aquarium);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
+app.MapPut("/aquariums/{id:int}", async (int id, Aquarium updates, FishContext db) =>
+{
+    var existing = await db.Aquariums.FindAsync(id);
+    if (existing == null)
+        return Results.NotFound();
+
+    existing.Name = updates.Name;
+    existing.Location = updates.Location;
+    existing.Capacity = updates.Capacity;
+
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
+app.MapDelete("/aquariums/{id:int}", async (int id, FishContext db) =>
+{
+    var existing = await db.Aquariums.FindAsync(id);
+    if (existing != null) db.Aquariums.Remove(existing);
+    return Results.Ok();
+});
 
 app.Run();
